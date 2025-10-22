8eaa083998660200e8fbcfbf7c957f36b9031613
Farzeensiddiquie <farzeenwasif15@gmail.com>
2025-10-22 22:26:54 +0500

    service cards

--- components/Services.js
+++ components/Services.js
@@ -5,15 +5,27 @@ export default function Services() {
     <div className='h-screen flex flex-col gap-20  items-center text-4xl font-bold'>
       <p>Services We Provide</p>
       <div className='flex justify-between w-280'>
-       <div class="w-70 h-80 rounded-2xl p-[1px]
+       <div className="w-70 h-80 rounded-2xl p-[1px]
     bg-gradient-to-r from-[#092e1b] to-[#1f7e4f]">
-  <div class="w-full h-full rounded-2xl
+  <div className="w-full h-full rounded-2xl
       bg-[radial-gradient(circle_at_top_right,_#055B30_0%,_rgba(4,28,17,0.4)_47%),linear-gradient(135deg,_rgba(4,28,0,2.71)_0%,_rgba(3,87,0,1.7)_100%)]">
     
   </div>
 </div>
-        <div className='w-70 h-80 bg-amber-500 rounded-2xl'></div>
-        <div className='w-70 h-80 bg-amber-500'></div>
+              <div className="w-70 h-80 rounded-2xl p-[1px]
+    bg-gradient-to-b from-[#092e1b] to-[#1f7e4f]">
+  <div className="w-full h-full rounded-2xl
+      bg-[radial-gradient(circle_at_bottom_left,_#055B30_0%,_rgba(4,28,17,0.4)_47%),linear-gradient(135deg,_rgba(4,28,0,2.71)_0%,_rgba(3,87,0,1.7)_100%)]">
+    
+  </div>
+</div>
+             <div className="w-70 h-80 rounded-2xl p-[1px]
+    bg-gradient-to-r from-[#092e1b] to-[#1f7e4f]">
+  <div className="w-full h-full rounded-2xl
+      bg-[radial-gradient(circle_at_top_right,_#055B30_0%,_rgba(4,28,17,0.4)_47%),linear-gradient(135deg,_rgba(4,28,0,2.71)_0%,_rgba(3,87,0,1.7)_100%)]">
+    
+  </div>
+</div>
 
       </div>
     </div>
